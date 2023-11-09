dd66243db6ef7d80d1f73f9ed87b4cfcf9e1a591
khadir malika <khadirmalika1997@gmail.com>
2023-11-09 23:46:20 +0100

    Ajout d'un titre

--- contact_travel/__manifest__.py
+++ contact_travel/__manifest__.py
@@ -1,21 +1,5 @@
-#{
-    #'name': 'Contact Travel',
-    #'version': '1.0',
-    #'summary': 'Gestion des voyages pour les contacts',
-    #'description': 'Ce module permet de gérer les voyages pour les contacts.',
-    #'category': 'Sales/CRM',
-    #'author': 'Malika',
-    #'website': 'www.contacttravel.com',
-    #'depends': ['base', 'crm'],
-    #'data': [
-        #'views/contact_travel_views.xml', w dima win ykoun files ta3 python drlhm __init__.py kima hadii win models
-    #],
-    #'installable': True,
-    #'application': True,
-#} ldork ma lgahouch hhhh pc ta3i thqil tani hhhh
 
-# -*- coding: utf-8 -*- bach n7otoh f depends, lzm dima DCdreri haka ki ta5dmi bl many2one wela on 2many t7oti module ta3hm f dependsdcr
-
+# -*- coding: utf-8 -*- 
 {   
     'name': "Contact Travel",
 
